c018a402df53ce9471fe63a54ffde3a2fa72ff70
Onur Asıliskender <onurasiliskender@gmail.com>
2018-04-05 15:31:32 +0300

    Menu Final 2
    Few updates on Final Menu Commit

--- Online User Manuel/CategoryItemViewController.swift
+++ Online User Manuel/CategoryItemViewController.swift
@@ -109,37 +109,3 @@ extension CategoryItemViewController:UITableViewDelegate, UITableViewDataSource 
     
 }
 
-extension UINavigationItem {
-
-    
-    func setTitle(title:String, subtitle:String) {
-        
-        let one = UILabel()
-        one.text = title
-        one.font = UIFont.systemFont(ofSize: 12)
-        one.textAlignment = .center
-        one.sizeToFit()
-        
-        let two = UILabel()
-        two.text = subtitle
-        two.font = UIFont.systemFont(ofSize: 17)
-        two.textAlignment = .center
-        two.sizeToFit()
-        
-        
-        
-        let stackView = UIStackView(arrangedSubviews: [one, two])
-        stackView.distribution = .equalCentering
-        stackView.axis = .vertical
-        
-        let width = max(one.frame.size.width, two.frame.size.width)
-        stackView.frame = CGRect(x: 0, y: 0, width: width, height: 35)
-        
-        one.sizeToFit()
-        two.sizeToFit()
-        
-        
-        
-        self.titleView = stackView
-    }
-}
--- Online User Manuel/DriversGuideViewController.swift
+++ Online User Manuel/DriversGuideViewController.swift
@@ -141,6 +141,39 @@ extension DriversGuideViewController:UITableViewDelegate, UITableViewDataSource 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return guideArray.count
     }
-    
-
 }
+
+extension UINavigationItem {
+    
+    func setTitle(title:String, subtitle:String) {
+        
+        let one = UILabel()
+        one.text = title
+        one.font = UIFont.systemFont(ofSize: 12)
+        one.textAlignment = .center
+        one.sizeToFit()
+        
+        let two = UILabel()
+        two.text = subtitle
+        two.font = UIFont.systemFont(ofSize: 17)
+        two.textAlignment = .center
+        two.sizeToFit()
+        
+        
+        
+        let stackView = UIStackView(arrangedSubviews: [one, two])
+        stackView.distribution = .equalCentering
+        stackView.axis = .vertical
+        
+        let width = max(one.frame.size.width, two.frame.size.width)
+        stackView.frame = CGRect(x: 0, y: 0, width: width, height: 35)
+        
+        one.sizeToFit()
+        two.sizeToFit()
+        
+        
+        
+        self.titleView = stackView
+    }
+}
+
