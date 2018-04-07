345cec64353ac3b0d71ed90139f0d6e863ee8c0f
Onur Asıliskender <onurasiliskender@gmail.com>
2018-04-08 00:59:45 +0300

    move file trash

--- Online User Manuel/File.swift
+++
@@ -1,9 +0,0 @@
-//
-//  File.swift
-//  Online User Manuel
-//
-//  Created by Onur Asıliskender on 8.04.2018.
-//  Copyright © 2018 uzum & asıliskender. All rights reserved.
-//
-
-import Foundation
