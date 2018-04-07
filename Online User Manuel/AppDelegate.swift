f84efda5f0c45efa44f2fc4f3d973c7b678d0f3d
Onur Asıliskender <onurasiliskender@gmail.com>
2018-04-08 01:34:53 +0300

    master commit

--- Online User Manuel/AppDelegate.swift
+++ Online User Manuel/AppDelegate.swift
@@ -8,10 +8,10 @@
 import UIKit
 
 @UIApplicationMain
+
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
@@ -31,6 +31,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
+        
+        
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
