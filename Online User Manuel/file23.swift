05b7c00975feae144dd977fb88416ac72956dee5
Onur Asıliskender <onurasiliskender@gmail.com>
2018-04-08 01:24:52 +0300

    hope last

--- Online User Manuel/file23.swift
+++
@@ -1,9 +0,0 @@
-//
-//  file23.swift
-//  Online User Manuel
-//
-//  Created by Onur Asıliskender on 8.04.2018.
-//  Copyright © 2018 uzum & asıliskender. All rights reserved.
-//
-
-import Foundation
